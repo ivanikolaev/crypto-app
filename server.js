6e264f0ce330b6a0117d3c51e5c8849708b5df2c
ivanikolaev <goturpokla@gmail.com>
2024-10-16 00:00:48 +0300

    json-server initial commit

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
-
-app.use(express.static('frontend/dist'))
-
-app.listen(port, () => console.log('Server has been started on port 80...'))
